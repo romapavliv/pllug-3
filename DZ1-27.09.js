f3189f17974608a4cb4b0b724400a07ad0bae2ec
Rost <bakun.rostyk@gmail.com>
2021-09-27 11:18:29 +0300

    DZ-1

---
+++ DZ1-27.09.js
@@ -0,0 +1,37 @@
+const arr = 'HellO woRlD!';
+
+function reverseCase(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i];
+
+        if(letter === letter.toLowerCase()){
+            result += letter.toUpperCase()
+        }else{
+            result += letter.toLowerCase()
+        }
+    }
+    return result
+    
+}
+console.log(reverseCase(arr));
+
+function crash1(){
+    let i = 1;
+    let a ;
+    while (i > 0){
+            a = i++;
+            console.log(a); 
+    }
+}
+
+crash1();
+
+
+function crach2(x) {
+    if (x >= 100000000)
+    return;
+    crach2(x + 1);
+}
+
+crach2(0);
