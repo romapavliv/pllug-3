e093a6ff0fc1716af23e015793e33bd126333b1e
Rost <bakun.rostyk@gmail.com>
2021-10-01 18:56:50 +0300

    Dz - part-I

---
+++ DZ4-30.09.js
@@ -0,0 +1,42 @@
+const number = [1,2,3]
+function alphabetnum(num) {
+   num = num.map((item)=> String.fromCharCode(item+64).toLowerCase())
+    return num
+}
+console.log(alphabetnum(number))
+/////////////////////
+
+const abc = ['FgG','abc','GTR',['zxc'], [1,2,3,-100],{name:'Rost'},' ' ];
+
+function trash(arr) {
+    arr = arr.flat();
+    let result =[];
+     for( let i = 0; i < arr.length; i++){
+         if(typeof arr[i] === 'string'){
+             result.push(arr[i])
+         }
+     }
+        let str = result.map((item) => item.toLowerCase());
+        str = str.map((item) => item.charAt(0).toUpperCase() + item.slice(1));
+
+        return [str.join('').trim()]
+
+    }
+console.log(trash(abc));
+/////////////////////
+
+const obj = {a: 22, b: -11.35, c: 41.2, d: 'hello'};
+
+function plus(obj) {
+    const keyVal = Object.entries(obj);
+    const filtered = keyVal.filter(([key, value]) => value >= 0);
+    // console.log(filtered);
+    const newObj = Object.fromEntries(filtered);
+         
+        return newObj
+}
+console.log(plus(obj));
+
+
+
+
--- note.js
+++ note.js
@@ -1,34 +1,14 @@
-const str = 'HellO woRlD!';
+const obj = {a: 22, b: -11.35, c: 41.2, d: 'hello'};
 
-function reverseCase(str) {
-    let result = "";
-    for (let i of str) {
-        let letter = i;
-
-    
-        result += (letter === letter.toLowerCase()) ? letter.toUpperCase() : letter.toLowerCase()
-    }
-    return result
-    
-}
-console.log(reverseCase(str));
-
-function crash1(){
-    let i = 1;
-    let a ;
-    while (i > 0){
-            a = i++;
-            console.log(a); 
-    }
+function plus(obj) {
+    const keyVal = Object.entries(obj);
+    const filtered = keyVal.filter(([key, value]) => value >= 0);
+    // console.log(filtered);
+    const newObj = Object.fromEntries(filtered);
+         
+        return newObj
 }
 
-crash1();
+console.log(plus(obj));
 
 
-function crach2(x) {
-    if (x >= 100000000)
-    return;
-    crach2(x + 1);
-}
-
-crach2(0);
