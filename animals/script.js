36c118a5032b9cc69ceae0c27883ff571f140fd6
Rost <bakun.rostyk@gmail.com>
2021-10-07 15:57:53 +0300

    animals-I

---
+++ animals/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+const animals = document.querySelector('#animals'),
+      cat = document.querySelector('#cat'),
+      pet = document.querySelectorAll('.pet');
+
+function fonSize(cls) { 
+    cls.style.fontSize = '30px';
+}
+fonSize(animals);
+
+function delLastCls(cls) {  
+    cls.lastElementChild.classList ='';
+}
+delLastCls(animals);
+
+function color(cls) { 
+    cls.nextElementSibling.style.color ='red';    
+}
+color(cat);
+
+// function DelElmByClass(cls) {
+//     while(cls.length > 0){
+//         cls[0].parentNode.removeChild(cls[0]);
+//     }
+// }
+// delLastCls(pet)
+
+
+
+
+
+
+console.log(pet);
+
+
+// console.log(animals);
+
+})
